5956f9969b22c0f3405cfb28d85df8f84b337935
Winlentia <winlentia@gmail.com>
2016-05-06 01:52:16 +0300

    bir başlangıc olsun diye birseyler yaptım , random numara ve isim olusturuyor , dosyalamaya falan yarın bakarık artıkın :D

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,45 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        System.out.println("Every body f.cking jump!");
+
+        System.out.println(generateRandomID());
+        System.out.println(generateRandomName());
     }
+
+    public static int generateRandomID()
+    {
+        int answer = randomWithRange(100000000,999999999);
+        return answer;
+    }
+
+    public static String generateRandomName()
+    {
+        Random r = new Random(); // just create one and keep it around
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        int N = randomWithRange(5,20);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
+        }
+        String randomName = sb.toString();
+        return randomName;
+    }
+
+
+
+    public static int randomWithRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
+
 }
