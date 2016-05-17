2404c46da5aff893a59f8b715c32e5a3489a4930
Winlentia <winlentia@gmail.com>
2016-05-18 01:36:01 +0300

    katlama , bölme atamaları tamam

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,43 +3,83 @@ package main;
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException
-    {
+/*
+      hash dosyalarının total boyutu 1217 olsun , asal sayı kullanılması daha iyi.
+     (tüm fonksiyonlar 1217 ye göre dizayn))
+ */
+public class Main {
+    public static int dataLength = 1217;
 
-        createRandomText(500);
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+        createRandomTextDataBase(500);
         //readFromFile();
         // createHashTable(); //Taslak Hash Table
 
     }
 
-    public static void createRandomText(int size) throws FileNotFoundException, UnsupportedEncodingException
-    {
+
+    public static void createRandomTextDataBase(int size) throws FileNotFoundException, UnsupportedEncodingException {
         PrintWriter writer = new PrintWriter("Users.txt", "UTF-8");
 
-        for (int i = 0; i < size; i++)
-        {
-            writer.println(generateRandomID() + ";" + generateRandomName() +  ";" + generateRandomName());
+        for (int i = 0; i < size; i++) {
+            writer.println(generateRandomID() + ";" + generateRandomName() + ";" + generateRandomName());
 
         }
         writer.close();
     }
 
-    public static int generateRandomID()
-    {
+    public static int bolenKalan(int number) {
+        return number % dataLength;
+    }
+
+    public static int katlama(int number) {
+        int num1, num2, num3;
+        int result = 0;
+        num1 = number % 1000;
+
+        result += convertNumberToOpposite(num1);
+        num2 = (number % 1000000 - num1) / 1000;
+        result += num2;
+        num3 = (number - number % 1000000) / 1000000;
+
+        result += convertNumberToOpposite(num3);
+        return result;
+    }
+
+    public static int convertNumberToOpposite(int number) {
+        int result = 0;
+        int j = 10;
+        for (int i = 0; i < numberOfLength(number); i++) {
+            result += ((number % j - number % (j / 10)) / (int) Math.pow(10, i)) * (int) Math.pow(10, numberOfLength(number) - i - 1);
+            j *= 10;
+        }
+        return result;
+
+    }
+
+    public static int numberOfLength(int number) {
+        int i = 1;
+        int j = 10;
+        while (number % j != number) {
+            i++;
+            j *= 10;
+        }
+        return i;
+
+    }
+
+    public static int generateRandomID() {
         return randomWithRange(100000000, 999999999);
     }
 
-    public static String generateRandomName()
-    {
+    public static String generateRandomName() {
         Random r = new Random(); // just create one and keep it around
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
         int N = randomWithRange(1, 10);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
         }
         int space = randomWithRange(5, N); // İsime space eklenecek !
@@ -48,14 +88,12 @@ public class Main
     }
 
 
-    public static int randomWithRange(int min, int max)
-    {
+    public static int randomWithRange(int min, int max) {
         int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
     }
 
-    public static void readFromFile()
-    {
+    public static void readFromFile() {
 
         // The name of the file to open.
         String file = "Users.txt";
@@ -65,8 +103,7 @@ public class Main
         // This will reference one line at a time
         String line = null;
 
-        try
-        {
+        try {
             // FileReader reads text files in the default encoding.
             FileReader fileRead =
                     new FileReader(file);
@@ -75,8 +112,7 @@ public class Main
             BufferedReader bufferedReader =
                     new BufferedReader(fileRead);
 
-            while ((line = bufferedReader.readLine()) != null)
-            {
+            while ((line = bufferedReader.readLine()) != null) {
 
                 String[] parts = line.split(";");
                 String stdNo = parts[0];
@@ -87,18 +123,14 @@ public class Main
 
                 //  System.out.println(name);
             }
-            System.out.print(hashDivision(Std_No));
+            //System.out.print(hashDivision(Std_No));
             // Always close files.
             bufferedReader.close();
-        }
-        catch (FileNotFoundException ex)
-        {
+        } catch (FileNotFoundException ex) {
             System.out.println(
                     "Unable to open file '" +
                             file + "'");
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(
                     "Error reading file '"
                             + file + "'");
@@ -107,122 +139,7 @@ public class Main
         }
     }
 
-    public static int hashDivision(List No)//Taslak
-    {
-        int[] foo = new int[20];
-        int i;
-        for (i = 0; i < No.size(); i++)
-        {
-            foo[i] = Integer.parseInt((String) No.get(i));
 
-        }
-        int[] modfoo = new int[20];
-        for (i = 0; i < 20; i++)
-        {
-            modfoo[foo[i] % 10] = Integer.parseInt((String) No.get(i));
-
-        }
-
-
-        return modfoo[1];
-    }
-
-    public static int hashPowMid(List No)//Taslak
-    {
-
-        int[] modPowMid = new int[20];
-        int[] powmid = new int[20];
-        int pow = 0;
-        int i = 0;
-        for (i = 0; i < No.size(); i++)
-        {
-            powmid[i] = Integer.parseInt((String) No.get(i));
-
-        }
-        int firstIn = 0;
-        pow = powmid[0] * powmid[0];
-        while (pow > 0)
-        {
-            pow = pow / 10;
-            firstIn++;
-        }
-        pow = powmid[0] * powmid[0];
-        int middle = ((firstIn + 1) / 2) - 1;
-        int[] powNum = new int[50];
-        int a = 1;
-        for (i = 1; i < firstIn; i++)
-        {
-            a = a * 10;
-        }
-        int temp = 0;
-        for (i = 0; i < firstIn; i++)
-        {
-            temp = pow / a;
-            powNum[i] = temp;
-            pow = pow % a;
-            a = a / 10;
-        }
-        int middStep = 0;
-        int midStepRight = 0;
-        middStep = powNum[middle];
-        modPowMid[0] = middStep * 10 + midStepRight;
-        modPowMid[0] = modPowMid[0] % 10;
-
-        return modPowMid[0];
-
-    }
-
-    public static int hashFolding(List No)//Taslak
-    {
-        int[] folding = new int[20];
-        int[] modFolding = new int[20];
-        int i = 0;
-        for (i = 0; i < No.size(); i++)
-        {
-            folding[i] = Integer.parseInt((String) No.get(i));
-
-        }
-        int num1, num2, num3;
-        int sum = 0;
-        num1 = folding[0] / 1000000;
-        folding[0] = folding[0] % 1000000;
-        num2 = folding[0] % 1000;
-        num3 = folding[0] / 1;
-        sum = num1 + num2 + num3;
-        modFolding[0] = sum % 10;
-        return modFolding[0];
-
-    }
-
-
-    public static void createHashTable()
-    {
-        // Create a hash map
-        Hashtable balance = new Hashtable();
-        Enumeration names;
-        String str;
-        double bal;
-
-        balance.put("Zara", new Double(3434.34));
-        balance.put("Mahnaz", new Double(123.22));
-        balance.put("Ayan", new Double(1378.00));
-        balance.put("Daisy", new Double(99.22));
-        balance.put("Qadir", new Double(-19.08));
-
-        // Show all balances in hash table.
-        names = balance.keys();
-        while (names.hasMoreElements())
-        {
-            str = (String) names.nextElement();
-            System.out.println(str + ": " +
-                    balance.get(str));
-        }
-        System.out.println();
-        // Deposit 1,000 into Zara's account
-        bal = ((Double) balance.get("Zara")).doubleValue();
-        balance.put("Zara", new Double(bal + 1000));
-        System.out.println("Zara's new balance: " + balance.get("Zara"));
-    }
 }
 
 
