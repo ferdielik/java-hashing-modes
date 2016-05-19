aafbe400f326c5f7a04578d0b48c39182a80848a
Ferdi Elik <elik.ferdi@gmail.com>
2016-05-20 01:02:04 +0300

    -

--- src/samples/Test.java
+++ src/samples/Test.java
@@ -13,10 +13,10 @@ public class Test
 
     Test()
     {
-        System.out.println("492715066 --  expected : 1075 folding index : " + hashing.findIndex(HashMode.FOLDING,  492715066));
-        System.out.println("492715066 --  expected : ? folding index : " + hashing.findIndex(HashMode.DIVIDING_THE_REMAINING,  492715066));
-        System.out.println("399630850 --  expected : ? folding index : " + hashing.findIndex(HashMode.MID_SQUARE,  399630850));
-        System.out.println("249220778 --  expected : ? folding index : " + hashing.findIndex(HashMode.MID_SQUARE,  249220778));
+        System.out.println("492715066 --  expected : 1075 folding index : " + hashing.findIndex(HashMode.FOLDING,  ConflictMode.LINEAR_PROBE, 492715066));
+        System.out.println("492715066 --  expected : ? folding index : " + hashing.findIndex(HashMode.DIVIDING_THE_REMAINING,  ConflictMode.LINEAR_PROBE,492715066));
+        System.out.println("399630850 --  expected : ? folding index : " + hashing.findIndex(HashMode.MID_SQUARE, ConflictMode.LINEAR_PROBE, 399630850));
+        System.out.println("249220778 --  expected : ? folding index : " + hashing.findIndex(HashMode.MID_SQUARE,ConflictMode.LINEAR_PROBE,  249220778));
 //        System.out.println("249220778 --  expected : ? folding index : " + hashing.find(HashMode.MID_SQUARE,  249220778));
 
         //        System.out.println("927150671 -- folding index : " + hashing.findIndexForSave(HashMode.FOLDING, ConflictMode.LINEAR_PROBE, 927150671));
