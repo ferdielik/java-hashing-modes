cb36ed1766614e1434fd26775528e6c08ec915c6
Ferdi Elik <elik.ferdi@gmail.com>
2016-05-20 00:08:37 +0300

    -

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,78 +22,35 @@ public class Main
         new Main();
     }
 
-    public Main()
+    private Integer test(HashMode hashMode, ConflictMode conflictMode)
     {
         hashFileController.createWorkBook();
         databaseController.createRandomTextDataBase(500);
-        buildHashFiles();
 
-        // 587363477;fdswvbcnnz;jwjveykcfa
-
-        Integer studentNumber = 197984916;
-//        System.out.println(hashing.get(HashMode.MID_SQUARE, ConflictMode.LINEAR_PROBE,studentNumber, true).toString());
-//        System.out.println(hashing.get(HashMode.DIVIDING_THE_REMAINING, ConflictMode.DISCRETE_OVERFLOW, studentNumber, true).toString());
-//
-
-        List<Student> students = databaseController.readFromFile();
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.DIVIDING_THE_REMAINING, ConflictMode.DISCRETE_OVERFLOW, studentNumber, true).toString());
-        }
-
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.MID_SQUARE, ConflictMode.DISCRETE_OVERFLOW,studentNumber, true).toString());
-        }
-
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.MID_SQUARE, ConflictMode.LINEAR_PROBE,studentNumber, true).toString());
-        }
-
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.FOLDING, ConflictMode.DISCRETE_OVERFLOW,studentNumber, true).toString());
-        }
-
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.FOLDING, ConflictMode.LINEAR_PROBE,studentNumber, true).toString());
-        }
-
-        for(Student student : students)
-        {
-            studentNumber = student.getId();
-            System.out.println(hashing.get(HashMode.DIVIDING_THE_REMAINING, ConflictMode.LINEAR_PROBE, studentNumber, true).toString());
-        }
-
-    }
-
-    private void buildHashFiles()
-    {
         List<Student> studentList = databaseController.readFromFile();
         for (Student student : studentList)
         {
-            //            dividingTheRemainingHashing.addWithDiscreteOverflow(student);
-            hashing.save(HashMode.DIVIDING_THE_REMAINING, ConflictMode.DISCRETE_OVERFLOW, student);
-            hashing.save(HashMode.DIVIDING_THE_REMAINING, ConflictMode.LINEAR_PROBE, student);
-
-            //            midSquareHashing.addWithDiscreteOverflow(student);
-            hashing.save(HashMode.MID_SQUARE, ConflictMode.DISCRETE_OVERFLOW, student);
-            hashing.save(HashMode.MID_SQUARE, ConflictMode.LINEAR_PROBE, student);
-
-            //            foldingHashing.addWithDiscreteOverflow(student);
-            hashing.save(HashMode.FOLDING, ConflictMode.DISCRETE_OVERFLOW, student);
-            hashing.save(HashMode.FOLDING, ConflictMode.LINEAR_PROBE, student);
+            hashing.save(hashMode, conflictMode, student);
         }
+
+        for(Student student : studentList)
+        {
+            Integer studentNumber = student.getId();
+            System.out.println(hashing.get(HashMode.DIVIDING_THE_REMAINING, ConflictMode.DISCRETE_OVERFLOW, studentNumber, true).toString());
+        }
+
+        return 0;
     }
 
-
+    public Main()
+    {
+        test(HashMode.DIVIDING_THE_REMAINING, ConflictMode.DISCRETE_OVERFLOW);
+        test(HashMode.MID_SQUARE, ConflictMode.DISCRETE_OVERFLOW);
+        test(HashMode.MID_SQUARE, ConflictMode.LINEAR_PROBE);
+        test(HashMode.FOLDING, ConflictMode.DISCRETE_OVERFLOW);
+        test(HashMode.FOLDING, ConflictMode.LINEAR_PROBE);
+        test(HashMode.DIVIDING_THE_REMAINING, ConflictMode.LINEAR_PROBE);
+    }
 }
 
 
